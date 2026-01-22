ea87508eb82557af0ef3335e79be042e71fa803e
Mason <deceitfulmason1987@gmail.com>
2026-01-22 17:32:45 -0300

    Update cod16_4.sv

--- exercise01/cod16_4.sv
+++ exercise01/cod16_4.sv
@@ -1,14 +1,14 @@
 module cod16_4 (
-    logic input [15:0] X,
-    logic output Y,
-    logic output GATE
+    logic input [15:0]  X,
+    logic output        Y,
+    logic output        GATE
 );
 
     always_comb begin
 
+       Y = 4'b0000 //Initializes Y as a 4 bit value of 0 to avoid errors 
+
         
-
-
     end
 
 
